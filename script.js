8dbd1ab9062f5a7953697a08e0f1222d81d81ba0
Samira Talanchi <sm@Samiras-MacBook-Air.local>
2024-12-06 15:51:30 +0330

    add js faze 3

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// your code here...
 function loadGame() {
     state = 'X';
     gameState.innerHTML = `It's ${state}'s turn`;
     gameArray = ["","","","","","","","",""] ;
+    win = false;
 }
 
 function changeState() {
@@ -14,11 +16,16 @@ function changeState() {
 }
 
 function fillCell(element,cellIndex) {
-    if (gameArray[cellIndex] === '') {
-        gameArray[cellIndex] = state;
-        element.innerHTML = state;
-        changeState();
+    if (win == false) {
+        if (gameArray[cellIndex] === '') {
+            gameArray[cellIndex] = state;
+            element.innerHTML = state;
+            if(!handlerEndGame()) {
+                changeState();
+            } 
+        }
     }
+    
 }
 
 function restartGame() {
@@ -28,9 +35,42 @@ function restartGame() {
     loadGame();
 }
 
+function handlerEndGame() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
+    ];
+    for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (gameArray[a] === gameArray[b] && gameArray[a] === gameArray[c] && gameArray[a] !== "") {
+            gameState.innerHTML = `Player ${state} has won`;
+            win = true;
+            return true;
+        }
+    }
+    if (win == false) {
+        let counter = 0;
+        for (let i = 0; i < gameArray.length; i++) {
+            if(!gameArray[i] == "") {
+                counter++;
+            }
+        }
+        if (counter == 9) {
+            gameState.innerHTML = `Game ended in a draw`;
+            win = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+        
+}
+
 let state;
 let gameArray;
 let gameState = document.querySelector(".game--status");
+let win;
 
 loadGame();
 
@@ -38,7 +78,6 @@ const test = document.querySelector(".game--container").addEventListener("click"
     const cell = event.target;
     const cellIndex = cell.getAttribute("data-cell-index");
     fillCell(cell,cellIndex);
-    
 })
 
 document.querySelector(".game--restart").addEventListener("click", restartGame);
